276e08d40d46d27c782ed0291f92019899c08352
lidi3 <lidi3@100tal.com>
2020-09-06 12:54:17 +0800

    score

--- advisor/rules.go
+++ advisor/rules.go
@@ -434,7 +434,7 @@ func init() {
 		},
 		"COL.001": {
 			Item:     "COL.001",
-			Severity: "L1",
+			Severity: "L0",
 			Summary:  "不建议使用 SELECT * 类型查询",
 			Content:  `当表结构变更时，使用 * 通配符选择所有列将导致查询的含义和行为会发生更改，可能导致查询返回更多的数据。`,
 			Case:     "select * from tbl where id=1",
